ce909be408a87677ab7a289bbf1396608423068d
vitodursopettersson <vitodurso98@gmail.com>
2023-11-08 22:22:43 +0100

    feat: generazione biglietto

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ va applicato uno sconto del 40 % per gli over 65.
 
 'use strict'
 
+// Variabili elementi html
+const elementTicketName = document.getElementById('ticket__name');
+const elementTicketType = document.getElementById('ticket__type');
+const elementTicketPrice = document.getElementById('ticket__price');
+
+
 // Variabili input
 const fullName = document.getElementById('full-name');
 const kmTrip = document.getElementById('km-trip');
@@ -21,18 +27,28 @@ const createTicket = document.getElementById('create-ticket');
 
 //Calcolo del biglietto
 createTicket.addEventListener('click', function () {
-    let ticketPrice = 0.21 * kmTrip.value
-    console.log('prezzo intero =', ticketPrice)
+
+    let ticketPrice = 0.21 * kmTrip.value;
+    console.log('prezzo intero =', ticketPrice);
+    let ticketType
 
     if (ageRange.value === 'minorenne') {
-        ticketPrice = (ticketPrice - (ticketPrice / 100 * 20))
-        console.log('prezzo scontato del 20% =', ticketPrice)
+        ticketPrice = (ticketPrice - (ticketPrice / 100 * 20));
+        console.log('prezzo scontato del 20% =', ticketPrice);
+        ticketType = 'Biglietto Ridotto';
 
     } else if (ageRange.value === 'over65') {
-        ticketPrice = (ticketPrice - (ticketPrice / 100 * 40))
-        console.log('prezzo scontato del 40% =', ticketPrice)
+        ticketPrice = (ticketPrice - (ticketPrice / 100 * 40));
+        console.log('prezzo scontato del 40% =', ticketPrice);
+        ticketType = 'Biglietto Ridotto';
 
     } else {
-        console.log('sono over 65')
+        ticketType = ('Biglietto Standard');
     }
+
+    // Generazione del biglietto
+    elementTicketName.innerHTML = fullName.value;
+    elementTicketType.innerHTML = ticketType
+    elementTicketPrice.innerHTML = ticketPrice + "€"
 });
+
